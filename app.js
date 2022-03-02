1cb1860a98b9c2f1c576e68dbd8409171df43879
Kristen <kristen.marcinek@rutgers.edu>
2022-03-02 13:09:15 -0500

    testing images

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const cloudTwo = document.getElementById('cloud-two')
 const cloudThree = document.getElementById('cloud-three')
 const cloudFour = document.getElementById('cloud-four')
 const cloudFive = document.getElementById('cloud-five')
-const javascript = document.getElementById('javascript')
-const react = document.getElementById('react')
-const graphql = document.getElementById('graphql')
-
 
 function move() {
   const incrementer = window.scrollY
@@ -27,9 +23,6 @@ function move() {
   cloudFour.style.left = 20 + incrementer * -0.15 + '%'
   cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
   cloudFive.style.left = 60 + incrementer * 0.16 + '%'
-  javascript.style.left = 20 + incrementer * -2.2 + '%'
-  react.style.left = 26 + incrementer * -1.5 + '%'
-  graphql.style.left = 32 + incrementer * -1 + '%'
 }
 
 window.addEventListener('scroll', move)
