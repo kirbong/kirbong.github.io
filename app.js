f63fb03763e34dd56442d442a82014329f0a69d2
Kristen <kristen.marcinek@rutgers.edu>
2022-03-02 13:39:54 -0500

    reverting back to old design

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-const balloon = document.getElementById('air-balloon')
-const cloudOne = document.getElementById('cloud-one')
-const cloudTwo = document.getElementById('cloud-two')
-const cloudThree = document.getElementById('cloud-three')
-const cloudFour = document.getElementById('cloud-four')
-const cloudFive = document.getElementById('cloud-five')
-
-function move() {
-  const incrementer = window.scrollY
-
-  // the '10' refers to the height from the bottom of the sky section. We added this
-  // in the css file. Feel free to move all styling to the JS file if you prefer to keep the
-  // the '10' consistent.
-  balloon.style.bottom = 10 + incrementer * 0.1 + '%'
-
-  cloudOne.style.bottom = 40 + incrementer * 0.12 + '%'
-  cloudOne.style.left = 75 + incrementer * 0.1 + '%'
-  cloudTwo.style.bottom = 80 + incrementer * 0.14 + '%'
-  cloudTwo.style.left = 70 + incrementer * 0.15 + '%'
-  cloudThree.style.bottom = 60 + incrementer * 0.1 + '%'
-  cloudThree.style.left = 0 + incrementer * -0.12 + '%'
-  cloudFour.style.bottom = 70 + incrementer * 0.16 + '%'
-  cloudFour.style.left = 20 + incrementer * -0.15 + '%'
-  cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
-  cloudFive.style.left = 60 + incrementer * 0.16 + '%'
-}
-
-window.addEventListener('scroll', move)
-
-
-
-
-
-
-
-
-
-/* $(document).ready(function(){
+$(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
@@ -43,4 +6,4 @@ window.addEventListener('scroll', move)
             $('.navbar').removeClass("sticky");
         }
     })
-}); */
+});
